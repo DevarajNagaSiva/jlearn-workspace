20e615e6d831061c0a21515d2749f0958ed881d8
DevarajNagaSiva <devaraj.nagasiva@gmail.com>
2026-03-24 19:39:26 +0530

    Initial commit with jlearn project

---
+++ jlearn/src/test/java/com/demo/jlearn/JlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
